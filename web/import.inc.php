975327473daccdec43d610e3dd621d1deb89c305
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-06-14 20:58:39 +0200

    web/import.inc.php: adjust again the reason detection
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- web/import.inc.php
+++ web/import.inc.php
@@ -95,7 +95,7 @@ function import_result($buildid, $filename)
       $reason = "none";
     else {
 	$tmp = Array();
-	exec("tail -3 " . $thisbuildfinaldir . "build-end.log | grep '\*\*\*' | sed 's,make: .*/\(build\|toolchain\)/\([^/]*\)/.*,\\2,'", $tmp);
+	exec("tail -3 " . $thisbuildfinaldir . "build-end.log | grep 'make: \*\*\*' | sed 's,make: .*/\(build\|toolchain\)/\([^/]*\)/.*,\\2,'", $tmp);
 	if (trim($tmp[0]))
 	  $reason = $tmp[0];
 	else
